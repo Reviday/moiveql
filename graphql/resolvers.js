e53b9c77cd0c30f8b7eaf157ab8700b88f924c2b
reviday <reviday@naver.com>
2020-04-22 14:53:38 +0900

    #10 Delete Mutation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -6,7 +6,8 @@ const resolvers = {
     movie: (_, { id }) => getById(id)
   },
   Mutation: {
-    addMovie: (_, { name, score }) => addMovie(name, score)
+    addMovie: (_, { name, score }) => addMovie(name, score),
+    deleteMovie: (_, { id }) => deleteMovie(id)
   }
 };
 
