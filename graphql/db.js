217f18cba7895db24f32782af4be4ec685144566
reviday <reviday@naver.com>
2020-04-22 14:44:21 +0900

    #9 Creating first Mutation

--- graphql/db.js
+++ graphql/db.js
@@ -37,3 +37,14 @@ export const deleteMovie = (id) => {
     return false;
   }
 }
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    // id: `${movies.length + 1}`, // 해당 방식은 id값 부여에 있어서, 삭제 로직과 함께 수행할 시 중복의 여지가 있음.
+    id: movies[movies.length - 1].id + 1, // 이 방식으로 바꾸면, 최소한 겹치는 경우는 없을 것으로 보임(변수 처리 없이 시퀀스와 비슷하게 순회하게끔)
+    name, 
+    score
+  };
+  movies.push(newMovie);
+  return newMovie;
+}
